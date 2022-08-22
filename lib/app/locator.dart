0e4ba7fbfe0f59314d2fbb94dead0c5c49897221
“prospectkeys” <prospectkeys@gmail.com>
2022-08-22 17:31:50 -0600

    Remove extraneous

--- lib/app/locator.dart
+++ lib/app/locator.dart
@@ -1,10 +1,8 @@
 import 'package:get_it/get_it.dart';
 import '../services/pins.services.dart';
-import '../services/single_pin.services.dart';
 
 final locator = GetIt.instance;
 
 setupLocator() {
   locator.registerLazySingleton(() => PinsService());
-  locator.registerLazySingleton(() => SinglePinService("dummyId"));
 }
