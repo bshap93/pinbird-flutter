7c152a476a9804858ab131e7345837e2d8ef5224
“prospectkeys” <prospectkeys@gmail.com>
2022-08-27 17:53:08 -0600

    Added tag to the new pin panel view and viewmodel

--- lib/app/locator.dart
+++ lib/app/locator.dart
@@ -1,10 +1,12 @@
 import 'package:get_it/get_it.dart';
 import 'package:pinboard_clone/models/pin_data.dart';
 import 'package:pinboard_clone/services/pin_data.services.dart';
+import 'package:pinboard_clone/services/tag.data.services.dart';
 import '../services/pins.services.dart';
 
 final locator = GetIt.instance;
 
 setupLocator() {
   locator.registerLazySingleton(() => PinDataService());
+  locator.registerLazySingleton(() => TagService());
 }
--- lib/ui/new_pin_panel/new_pin_panel_viewmodel.dart
+++ lib/ui/new_pin_panel/new_pin_panel_viewmodel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pinboard_clone/services/tag.data.services.dart';
 import 'package:stacked/stacked.dart';
 
 import '../../app/locator.dart';
@@ -10,9 +11,10 @@ import '../../services/pins.services.dart';
 class NewPinPanelViewModel extends ReactiveViewModel {
   final _firstPinFocusNode = FocusNode();
   final _pinDataService = locator<PinDataService>();
-  // late final toggleStatus = _pinsService.toggleStatus;
-  // late final removePin = _pinsService.removePin;
   late final updatePinContent = _pinDataService.updatePinDataContent;
+
+  final _tagService = locator<TagService>();
+  late final newTag = _tagService.newTag;
 
   List<PinData> get pins => _pinDataService.pin_data;
 
